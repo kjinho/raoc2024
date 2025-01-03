0532c5def19ed7efc36ac856719cccb5b94e93ab
Jin-Ho King <j@kingesq.us>
2025-01-03 11:36:28 -0500

    working day2

--- src/day02.rs
+++ src/day02.rs
@@ -1,12 +1,62 @@
-// use std::{error::Error, num::ParseIntError};
+use std::{error::Error, num::ParseIntError};
 
 
-// struct Report(Vec<isize>);
+struct Report(Vec<isize>);
 
-// // fn parse_report(s: &str) -> Result<Report,ParseIntError> {
-// //     s.split_whitespace()
-// //         .map(|x|x.parse::<isize>())
-// //         .try_collect::<Vec<isize>>()
-// //         .map(Report)
-// // }
+fn parse_report(s: &str) -> Result<Report,ParseIntError> {
+    s.split_whitespace()
+        .map(|x|x.parse::<isize>())
+        .collect::<Result<Vec<isize>,_>>()
+        .map(Report)
+}
 
+fn parse_input(input: &str) -> Result<Vec<Report>, ParseIntError> {
+  input.lines().map(parse_report).collect::<Result<Vec<Report>,ParseIntError>>()
+}
+
+fn all_increasing_p(Report(level): &Report) -> bool {
+  level.iter().is_sorted_by(| &a, &b| *a < *b)
+}
+
+fn all_decreasing_p(Report(level): &Report) -> bool {
+  level.iter().is_sorted_by(| &a, &b| *a > *b)
+}
+
+fn permitted_diff_p(a: isize, b: isize) -> bool {
+  let diff = a.abs_diff(b);
+  diff >= 1 && diff <= 3
+}
+
+fn permitted_diff2_p(nums: &[isize]) -> bool {
+  match nums {
+    [first, second] => permitted_diff_p(*first, *second),
+    _ => false
+  }
+}
+
+fn all_permitted_diff(Report(levels): &Report) -> bool {
+  levels.windows(2).all(permitted_diff2_p)
+}
+
+fn level_safe_p(report: &Report) -> bool {
+  (all_increasing_p(report) || all_decreasing_p(report)) && all_permitted_diff(report)
+}
+
+pub fn part1(input: String) -> Result<usize,Box<dyn Error>> {
+  Ok(parse_input(input.as_str())?.iter().filter(|&x| level_safe_p(x)).count())
+}
+
+fn dampened_safe_p(Report(levels): &Report) -> bool {
+  for (i,_) in levels.iter().enumerate() {
+    let mut vec = levels.clone();
+    vec.remove(i);
+    if level_safe_p(&Report(vec)) {
+      return true
+    }
+  }
+  false
+}
+
+pub fn part2(input: String) -> Result<usize,Box<dyn Error>> {
+  Ok(parse_input(input.as_str())?.iter().filter(|&x| dampened_safe_p(x)).count())
+}
--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,9 @@ fn load_input(day: usize, part: usize) -> io::Result<String> {
 fn main() -> Result<(), Box<dyn Error>> {
     for (day, part,day_func) in [
         (1usize,1usize,day01::part1 as fn(String) -> Result<usize,Box<dyn Error>>),
-        (1,2,day01::part2 as fn(String) -> Result<usize,Box<dyn Error>>)
+        (1,2,day01::part2 as fn(String) -> Result<usize,Box<dyn Error>>),
+        (2,1,day02::part1 as fn(String) -> Result<usize,Box<dyn Error>>),
+        (2,2,day02::part2 as fn(String) -> Result<usize,Box<dyn Error>>),
     ] {
         println!("Result for Day {}, Part {}", day, part);
         println!("| Output A: {}", day_func(load_input(day, 1)?)?);
