1fbf050d4b5416a85cf6af59b5408876f31b4862
Jin-Ho King <j@kingesq.us>
2025-01-03 23:47:31 -0500

    replaced regex with nom

--- src/day03.rs
+++ src/day03.rs
@@ -1,80 +1,102 @@
 use std::error::Error;
-use regex::Regex;
+use nom::{
+  branch::alt, 
+  bytes::complete::tag, 
+  character::complete::{anychar, char, u64}, 
+  multi::many0, 
+  sequence::{delimited, preceded, separated_pair}, 
+  IResult
+};
 
+#[derive(Clone, Copy, Debug)]
 struct Mul(usize,usize);
+
+#[derive(Clone, Copy, Debug)]
 enum Command {
   Multiply(Mul),
   Dont,
-  Do
+  Do,
+  Nop
 }
 
-const MULTIPLY_RE: &str = r"mul\((\d+),(\d+)\)";
-const ALL_COMMANDS_RE: &str = r"(mul\(\d+,\d+\)|don[']t\(\)|do\(\))";
+fn parse_dont(input: &str) -> IResult<&str, Command> {
+  let (tail, _) = tag("don't()")(input)?;
+  Ok((tail, Command::Dont))
+}
 
-fn parse_input(input: String) -> Result<Vec<Mul>,Box<dyn Error>> {
-  let re = Regex::new(MULTIPLY_RE).unwrap();
-  let mut result: Vec<Mul> = Vec::new();
-  for (_, [a,b]) in re.captures_iter(input.as_str()).map(|c| c.extract()) {
-    let a2: usize  = a.parse()?;
-    let b2: usize = b.parse()?;
-    result.push(Mul(a2,b2));
+fn parse_do(input: &str) -> IResult<&str, Command> {
+  let (tail, _) = tag("do()")(input)?;
+  Ok((tail, Command::Do))
+}
+
+fn parse_comma_num(input: &str) -> IResult<&str,(usize,usize)> {
+  let (tail, (a, b)) = 
+    separated_pair(u64,char(','),u64)(input)?;
+  Ok((tail, (a as usize, b as usize)))
+}
+
+fn parse_mul(input: &str) -> IResult<&str, Command> {
+  let (tail, (first, second)) = 
+    preceded(tag("mul"),delimited(char('('),parse_comma_num,char(')')))(input)?;
+  Ok((tail, Command::Multiply(Mul(first, second))))
+}
+
+fn parse_anything_else(input: &str) -> IResult<&str,Command> {
+  let (tail, _) = anychar(input)?;
+  Ok((tail, Command::Nop))
+}
+
+fn parse_ops_part2(input: &str) -> IResult<&str, Command> {
+  alt((
+    parse_dont,
+    parse_do,
+    parse_mul,
+    parse_anything_else
+  ))(input)
+}
+
+fn parse_input(input: &str) -> IResult<&str,Vec<Command>> {
+  let (_, result) = many0(parse_ops_part2)(input)?;
+  Ok(("", result))
+}
+
+fn exec_command_part1(command: Command, _: bool) -> (usize,bool) {
+  match command {
+    Command::Multiply(Mul(a,b)) => (a*b,true),
+    _ => (0,true)
   }
-  Ok(result)
 }
 
-pub fn part1(input: String) -> Result<usize,Box<dyn Error>> {
-  let result = 
-    parse_input(input)?
-      .iter()
-      .map(|Mul(a,b)| a*b)
-      .sum::<usize>();
-  Ok(result)
-}
-
-fn parse_mul(input: &str) -> Result<Mul, Box<dyn Error>> {
-  let length = input.chars().count();
-  let result = 
-    input.chars().skip(4)
-      .take(length - 5)
-      .collect::<String>();
-  let mut split_string = result.as_str()
-      .split(',');
-  let a = split_string.next().ok_or("Nothing found")?;
-  let b = split_string.next().ok_or("Only one value found")?;
-  let a2: usize = a.parse()?;
-  let b2: usize = b.parse()?;
-  Ok(Mul(a2,b2))
-}
-
-fn parse_part2(input: String) -> Result<Vec<Command>,Box<dyn Error>> {
-  let re = Regex::new(ALL_COMMANDS_RE).unwrap();
-  let mut result: Vec<Command> = Vec::new();
-  for (_, [command]) in re.captures_iter(input.as_str()).map(|c| c.extract()) {
-    match command {
-      "don't()" => 
-        result.push(Command::Dont),
-      "do()" => 
-        result.push(Command::Do),
-      _ if command.starts_with("mul(") => 
-        result.push(Command::Multiply(parse_mul(command)?)),
-      _ => continue
-    }
+fn exec_command_part2(command: Command, state: bool) -> (usize,bool) {
+  match command {
+    Command::Do => (0,true),
+    Command::Dont => (0,false),
+    Command::Multiply(Mul(a,b)) => if state {
+      (a*b,true)
+    } else {
+      (0,false)
+    },
+    _ => (0,state)
   }
-  Ok(result)
 }
 
-pub fn part2(input: String) -> Result<usize,Box<dyn Error>> {
-  let parsed = parse_part2(input)?;
+fn part_helper(input: String, command_fn: fn(Command,bool)->(usize,bool)) -> Result<usize,Box<dyn Error>> {
+  let (_, parsed) = 
+    parse_input(&input).map_err(|e| format!("Parsing error: {:?}", e))?;
   let mut aggregate: usize = 0;
   let mut aggregate_p: bool = true;
   for command in parsed {
-    match command {
-      Command::Do => aggregate_p = true,
-      Command::Dont => aggregate_p = false,
-      Command::Multiply(Mul(a,b)) => if aggregate_p {
-        aggregate += a*b;
-      }
-    }
+    let (new_num, new_aggregate_p) = command_fn(command, aggregate_p);
+    aggregate += new_num;
+    aggregate_p = new_aggregate_p;
   }
   Ok(aggregate)
+}
+
+pub fn part1(input: String) -> Result<usize,Box<dyn Error>> {
+  part_helper(input, exec_command_part1)
+}
+
+pub fn part2(input:String) -> Result<usize,Box<dyn Error>> {
+  part_helper(input, exec_command_part2)
 }
